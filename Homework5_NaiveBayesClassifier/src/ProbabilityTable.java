5f9cd85f1a0dde6d582292081f2fdb046e834a66
Dimitrina Vasileva <dimitrina.vasileva@lights.digital>
2021-02-01 12:34:53 +0200

    Implement HW5 using Naive Bayes Classifier

---
+++ Homework5_NaiveBayesClassifier/src/ProbabilityTable.java
@@ -0,0 +1,17 @@
+public class ProbabilityTable {
+    private double[][] table;
+
+    public ProbabilityTable(int[] yes, int[] no, int[] other) {
+        table = new double[3][2];
+        table[0][0] = (double) yes[0] / (yes[0] + yes[1]);
+        table[0][1] = (double) yes[1] / (yes[0] + yes[1]);
+        table[1][0] = (double) no[0] / (no[0] + no[1]);
+        table[1][1] = (double) no[1] / (no[0] + no[1]);
+        table[2][0] = (double) other[0] / (other[0] + other[1]);
+        table[2][1] = (double) other[1] / (other[0] + other[1]);
+    }
+
+    public double[][] getTable() {
+        return table;
+    }
+}
